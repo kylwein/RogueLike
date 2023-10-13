eada99eda0ca4250b08faf3d7810d1b908f83e9f
Matheus Maldaner <matheus@sisnema.com.br>
2023-10-12 23:13:37 -0400

    displays player

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
 # creates the game window (we can change these values)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+RED = (255, 0, 0)
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pygame
 import constants
-import character
+from character import Character
+
 
 # initializes pygame
 pygame.init()
 
+# sets screen dimensions
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 pygame.display.set_caption("Jason Journey")
+
+# creates our hero!
+player = Character(100, 100)
+
 
 # keeps window open while True
 run = True
 while run:
+
+  # displays the player (Jason!)
+  player.draw(screen)
+
 
   # event handler
   for event in pygame.event.get():
@@ -18,6 +28,10 @@ while run:
     # finishes loop when you close the game window
     if event.type == pygame.QUIT:
       run = False
+
+
+
+  pygame.display.update()
 
 # quits pygame
 pygame.quit()
