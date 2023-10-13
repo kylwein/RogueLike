d3bb95c05397e2aeb49e08e57737717a5a249dd3
Matheus Maldaner <matheus@sisnema.com.br>
2023-10-12 21:45:08 -0400

    added scripts

---
+++ constants.py
@@ -0,0 +1,3 @@
+# creates the game window (we can change these values)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+import constants
+
+# initiliazes pygame
+pygame.init()
+
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+pygame.display.set_caption("Jason Journey")
+
+# keeps window open while True
+run = True
+while run:
+
+  # event handler
+  for event in pygame.event.get():
+
+    # finishes loop when you close the game window
+    if event.type == pygame.QUIT:
+      run = False
+
+# quits pygame
+pygame.quit()
