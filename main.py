2d8d635cd662a2a4af41db32cbec6449fd3c1cd7
Matheus Maldaner <matheus@sisnema.com.br>
2023-10-20 23:03:30 -0400

    displays player

--- main.py
+++ main.py
@@ -30,6 +30,22 @@ while run:
     if event.type == pygame.QUIT:
       run = False
 
+    if event.type == pygame.KEYDOWN:
+      # moves right
+      if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+        print("right")
+
+      # moves left
+      if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+        print("left")
+
+      # moves up
+      if event.key == pygame.K_w or event.key == pygame.K_UP:
+        print("up")
+
+      # moves down
+      if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+        print("down")
 
 
   pygame.display.update()
