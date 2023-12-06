4f5fbe693b7d774652e836a61905c923e954fb76
victoriamanzato <145578820+victoriamanzato@users.noreply.github.com>
2023-12-05 23:56:04 -0500

    Update button.py
    made button clickable

--- button.py
+++ button.py
@@ -7,7 +7,20 @@ class Button():
     self.rect.topleft = (x,y)
 
 def draw(self,surface):
+  action = False
+
+  #get mouse position
+  pos = pygame.mouse.get_pos()
+
+#check mouseover and clicked conditions
+  if self.rect.collidepoint(pos):
+    if pygame.mouse.get_pressed()[0]:
+      action = True
+
   surface.blit(self.image, self.rect)
+
+  return action
+
 
   
   
